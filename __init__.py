f96de4df53a909e6b273b37b630ea66fedc210f3
Charlie Li <git@vishwin.info>
2014-10-09 13:46:15 -0400

    Sanitise the first step

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.4
-from flask import Flask
-
-app=Flask(__name__)
-app.config_from_object('config')
-
-import views
--- views.py
+++ views.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3.4
-from . import app
-from flask import render_template
+from flask import Flask, render_template
+
+app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return 'Just messing around. Nothing to see yet.'
+
+@app.route('/<page>')
+def get_page(page):
+	return 'This is where ' + page + ' would display.'
+
+if __name__=='__main__':
+	app.run()
