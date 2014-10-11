70039b9afb04b508dfbdc43584c7201377dc366a
Charlie Li <git@vishwin.info>
2014-10-11 18:41:55 -0400

    Add the SASS

--- views.py
+++ views.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3.4
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask.ext.libsass import *
+import pkg_resources
 
 app=Flask(__name__)
+
+Sass(
+	{'app': 'scss/app.scss'},
+	app,
+	url_path='/static/css',
+	include_paths=[pkg_resources.resource_filename('views', 'scss')],
+	output_style='compressed'
+)
 
 @app.route('/')
 def index():
