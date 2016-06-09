ab7bcbdd705d553f4eaa6e98bff1d860343dc1f1
Charlie Li <git@vishwin.info>
2016-06-09 03:32:20 -0400

    403

--- vishwin_http/views.py
+++ vishwin_http/views.py
@@ -26,6 +26,10 @@ Sass(
 def error_404(error):
 	return render_template('404.html'), 404
 
+@app.errorhandler(403)
+def error_403(error):
+	return render_template('403.html'), 403
+
 @app.route('/static/js/<js>')
 def render_js(js):
 	try:
