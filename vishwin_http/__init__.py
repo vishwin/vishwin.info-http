fc0d61fc4d8fe960392855e5f37030572cac3b4d
Charlie Li <git@vishwin.info>
2016-06-13 09:33:03 -0400

    Use filesystem cache
    - Switch to filesystem cache to make sure existing JS cache still works
    - Set timeout to one hour

--- vishwin_http/__init__.py
+++ vishwin_http/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from flask import Flask
-from werkzeug.contrib.cache import SimpleCache
+from werkzeug.contrib.cache import FileSystemCache
+import pkg_resources
 
 app=Flask(__name__)
 #app.config.from_object('config')
 
-cache=SimpleCache()
+cache=FileSystemCache(pkg_resources.resource_filename('vishwin_http', 'cache'), default_timeout=60 * 60) # one hour timeout
 
 import vishwin_http.views
