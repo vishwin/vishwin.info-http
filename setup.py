37f22b3ea287566f30201106b3ab23b9bba5706b
Charlie Li <git@vishwin.info>
2017-05-09 00:38:51 -0400

    Refine setuptools
    - Fill out PKG-INFO more
    - Include empty directory structures for generated web assets
    - Fix typos

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
 	name='vishwin-http',
-	description='The vishwin.info HTTP interface',
 	version='17.05.09',
+	description='The vishwin.info HTTP interface',
+	url='https://git.vishwin.info/site/http.git/',
+	author='Charlie Li',
+	license='MPL-2.0',
 	packages=find_packages(),
 	install_requires=['Flask', 'markdown', 'slimit', 'Pillow', 'exifread', 'pylibmc'],
 	setup_requires=['libsass>=0.6.0'],
-	sass_manifests={'vishwin_http': ('sass', 'generated/css', 'static/css')},
+	sass_manifests={'vishwin_http': ('scss', 'generated/css', 'static/css')},
 	include_package_data=True,
 	zip_safe=False
 )
